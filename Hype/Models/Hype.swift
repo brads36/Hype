afdfef09da26f0b42b1810455803937f87007cd2
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-05-11 11:27:04 -0600

    File Structure created, CloudKit enabled

---
+++ Hype/Models/Hype.swift
@@ -0,0 +1,53 @@
+//
+//  Hype.swift
+//  Hype
+//
+//  Created by Bryce Bradshaw on 5/11/20.
+//  Copyright © 2020 Bryce Bradshaw. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+struct HypeStrings {
+    
+    static let recordTypeKey = "Hype"
+    static let bodyKey = "body"
+    static let timestampKey = "timestamp"
+    
+}
+
+class Hype {
+    
+    var body: String
+    var timestamp: Date
+    
+    init(body: String, timestamp: Date = Date()) {
+        self.body = body
+        self.timestamp = timestamp
+    }
+}
+
+// MARK: - Hype Convenience init
+extension Hype {
+    convenience init?(ckRecord: CKRecord) {
+        guard let body = ckRecord[HypeStrings.bodyKey] as? String,
+            let timestamp = ckRecord[HypeStrings.timestampKey] as? Date
+            else { return nil }
+        self.init(body: body, timestamp: timestamp)
+    }
+    
+}
+
+// MARK: - CKRecord Convenience initializer
+extension CKRecord {
+    
+    convenience init(hype: Hype) {
+        self.init(recordType: HypeStrings.recordTypeKey)
+        
+        self.setValuesForKeys([
+            HypeStrings.bodyKey : hype.body,
+            HypeStrings.timestampKey : hype.timestamp
+        ])
+    }
+}
